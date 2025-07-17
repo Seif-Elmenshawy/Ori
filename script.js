fee43ee5efa49a45118509b03028aac5e7fcfbbb
Seif-Elmenshawy <seif.ahmed.3849@gmail.com>
2025-07-17 03:04:40 +0300

    pic

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         const welcomeElement = document.getElementById("welcome");
             welcomeElement.classList.add("after-time");
+        getElementById("hero").style.display="block";
     }, 1500);
 });
