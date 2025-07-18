9d3ab9935146b15f95ed0175adf49a196367b6c4
Seif-Elmenshawy <seif.ahmed.3849@gmail.com>
2025-07-18 04:14:04 +0300

    audio

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         const welcomeElement = document.getElementById("welcome");
             welcomeElement.classList.add("after-time");
-        getElementById("hero").style.display="block";
+        document.getElementById("hero").style.display="block";
     }, 1500);
 });
 function skillsNav(){
