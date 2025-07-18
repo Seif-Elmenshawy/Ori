6b48f30cefe6036dd8558e9627f2fb2434b2883c
Seif-Elmenshawy <seif.ahmed.3849@gmail.com>
2025-07-18 05:07:41 +0300

    كفاياي النهاردة عشان انام

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("hero").style.display="block";
     }, 1500);
 });
+function hobbyNav(){
+    document.getElementById("hobbies-section").style.display="flex";
+    document.getElementById("skills-section").style.display="none";
+    document.getElementById("skills").classList.remove("active");
+    document.getElementById("skills").classList.add("nav-item");
+    document.getElementById("hobbies").classList.remove("nav-item");
+    document.getElementById("hobbies").classList.add("active");
+}
 function skillsNav(){
     document.getElementById("skills-section").style.display="flex";
     document.getElementById("hobbies-section").style.display="none";
-    document.getElementById("skills").classList.add("active");
+    document.getElementById("hobbies").classList.remove("active");
     document.getElementById("hobbies").classList.add("nav-item");
+    document.getElementById("skills").classList.remove("nav-item");
+    document.getElementById("skills").classList.add("active");
 }
