8da20e9875b5864be56589533d216f2092814ab5
Seif-Elmenshawy <seif.ahmed.3849@gmail.com>
2025-07-19 03:36:44 +0300

    finished

--- script.js
+++ script.js
@@ -21,3 +21,24 @@ function skillsNav(){
     document.getElementById("skills").classList.remove("nav-item");
     document.getElementById("skills").classList.add("active");
 }
+function projectsNav() {
+    document.getElementById("about").style.display="none";
+    document.getElementById("project").style.display="flex";
+    document.getElementById("projects").classList.remove("nav-item");
+    document.getElementById("projects").classList.add("active");
+    document.getElementById("aboutme").classList.remove("active");
+    document.getElementById("aboutme").classList.add("nav-item");
+    document.getElementById("contactme").classList.remove("active");
+    document.getElementById("contactme").classList.add("nav-item");
+}
+function aboutNav(){
+    document.getElementById("about").style.display="flex";
+    document.getElementById("project").style.display="none";
+    document.getElementById("contact").style.display="none";
+    document.getElementById("projects").classList.remove("active");
+    document.getElementById("projects").classList.add("nav-item");
+    document.getElementById("aboutme").classList.remove("nav-item");
+    document.getElementById("aboutme").classList.add("active");
+    document.getElementById("contactme").classList.remove("active");
+    document.getElementById("contactme").classList.add("nav-item");
+}
